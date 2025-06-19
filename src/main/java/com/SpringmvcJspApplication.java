ac96e208488916932c77b1f96884751fdfd16633
phamtranthuan2003 <phamtranthuan2003@gmail.com>
2025-06-19 17:28:18 +0700

    2

--- src/main/java/com/SpringmvcJspApplication.java
+++ src/main/java/com/SpringmvcJspApplication.java
@@ -2,8 +2,10 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.entity"})
 public class SpringmvcJspApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringmvcJspApplication.class, args);
