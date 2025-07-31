12ea8c30c1feed1aa3e4cb7555a1894e88099e42
phamtranthuan2003 <phamtranthuan2003@gmail.com>
2025-07-31 09:23:56 +0700

    UD

--- src/main/java/com/controller/user/CustomerController.java
+++ src/main/java/com/controller/user/CustomerController.java
@@ -5,10 +5,6 @@ import com.entity.common.Role;
 import com.repository.CustomerRepository;
 import com.service.ForgotPasswordService;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
